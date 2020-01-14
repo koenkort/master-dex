13e28795422bfd02a37ff03e4933e5a3790f38bf
kmensink95 <kmensink95@gmail.com>
2020-01-14 11:20:31 +0100

    setbackground color for pokecards

--- front-dex/src/app/components/card/card.component.ts
+++ front-dex/src/app/components/card/card.component.ts
@@ -15,4 +15,49 @@ export class CardComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  setBackgroundColor(types) {
+    const className: string = 'pokecard';
+
+    let expression = types.length === 2 ?  types[1].type.name :  types[0].type.name;
+
+    switch(expression) {
+      case 'normal':
+        return className+'--normal';
+      case 'fire':
+        return className+'--fire';
+      case 'fighting':
+        return className+'--fighting';
+      case 'water':
+        return className+'--water';
+      case 'flying':
+        return className+'--flying';
+      case 'grass':
+        return className+'--grass';
+      case 'poison':
+        return className+'--poison';
+      case 'electric':
+        return className+'--electric';
+      case 'ground':
+        return className+'--ground';
+      case 'psychic':
+        return className+'--psychic';
+      case 'rock':
+        return className+'--rock';
+      case 'ice':
+        return className+'--ice';
+      case 'bug':
+        return className+'--bug';
+      case 'dragon':
+        return className+'--dragon';
+      case 'ghost':
+        return className+'--ghost';
+      case 'dark':
+        return className+'--dark';
+      case 'steel':
+        return className+'--steel';
+      case 'fairy':
+        return className+'--fairy';
+      }
+  }
 }
