3e773a52253efde1ccd3f02225d3ed4395e22bc9
koenkort <koenkortw@gmail.com>
2020-01-15 11:12:11 +0100

    Changed default route to home

--- front-dex/src/app/app-routing.module.ts
+++ front-dex/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
 ];
