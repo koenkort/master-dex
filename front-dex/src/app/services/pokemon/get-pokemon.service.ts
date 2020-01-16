aaf4d574e84fd9aa3faf0266c70d38e1c82d782f
Danny Mus <dannymus@competa.com>
2020-01-16 12:08:06 +0100

    reformated const, and added newlines

--- front-dex/src/app/services/pokemon/get-pokemon.service.ts
+++ front-dex/src/app/services/pokemon/get-pokemon.service.ts
@@ -12,16 +12,16 @@ export class PokemonService {
 
     getPokemon(): Observable<Pokemon[]> {
         const noGen = 0;
-        const genOne = 151;
-        const genTwo = 251;
-        const genThree = 386;
-        const genFour = 493;
-        const genFive = 649;
-        const genSix = 721;
-        const genSeven = 807;
+        const gen1 = 151;
+        const gen2 = 251;
+        const gen3 = 386;
+        const gen4 = 493;
+        const gen5 = 649;
+        const gen6 = 721;
+        const gen7 = 807;
         const pokemonData: Observable<Pokemon>[] = [];
 
-        for(let i = noGen+1; i <= genOne; i++) {
+        for(let i = noGen+1; i <= gen1; i++) {
             pokemonData.push(this.http.get<Pokemon>(`http://pokeapi.co/api/v2/pokemon/${i}`));
         }
         return zip(...pokemonData);
