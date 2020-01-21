eae882df6ff4b969f8e5a645a807ace6fced004a
dannyvanderwel <dannyvdwel@competa.com>
2020-01-21 10:17:55 +0100

    made the login endpoint

--- app/models/team_pokemon.py
+++ app/models/team_pokemon.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class TeamPokemon(models.Model):
-    pokemnon_id = models.IntegerField()
+    pokemon_id = models.IntegerField(default=0)
     team = models.ForeignKey('Team', related_name="pokemon", on_delete=models.CASCADE)
