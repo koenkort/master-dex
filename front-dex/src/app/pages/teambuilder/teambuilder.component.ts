794f204d319eb8c46c1a125585d007d8300c2790
Danny Mus <dannymus@competa.com>
2020-01-21 15:46:16 +0100

    added basic functionalitie and styling for team builder

--- front-dex/src/app/pages/teambuilder/teambuilder.component.ts
+++ front-dex/src/app/pages/teambuilder/teambuilder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Pokemon from 'src/app/models/interfaces/Pokemon';
+import { PokemonService } from '../../services/pokemon/get-pokemon.service';
 
 @Component({
   selector: 'app-teambuilder',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teambuilder.component.scss']
 })
 export class TeambuilderComponent implements OnInit {
+  pokemons: Pokemon[];
 
-  constructor() { }
+  constructor(private PokemonService: PokemonService){}
+
+  getData():void {
+    this.PokemonService.getPokemon().subscribe(pokemons => this.pokemons = pokemons)
+  }
 
   ngOnInit() {
+    this.getData();
   }
 
 }
